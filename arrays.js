96bff9378633803a2d9c134a9697fcd8ab7a0109
Eric Lau <yasuyo369@gmail.com>
2017-02-13 23:04:35 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -1,6 +1,46 @@
-var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
+function removeElementFromBeginningOfArray(array)
+{ array.shift();
+return array;}
 
-function addElementToEndOfArray((a=["Tom", "Sam"]), (b=Sandy)) {
-  var c = a.unshift(b);
-  return c;
+function removeElementFromEndOfArray(array)
+{
+  array.pop();
+  return array;
+}
+
+function destructivelyAddElementToEndOfArray(array, element)
+{
+  array.push(element);
+  return array;
+}
+
+function addElementToBeginningOfArray(array, element)
+{
+  array = [element, ...array];
+  return array;
+}
+
+function addElementToEndOfArray(array, element)
+{
+  array= [...array, element];
+  return array;
+
+}
+
+function destructivelyAddElementToEndOfArray(array, element)
+{
+  array.push(element);
+  return array;
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element)
+{
+  array.unshift(element);
+  return array;
+}
+
+function accessElementInArray(array, index)
+{
+  return array[index];
 }
