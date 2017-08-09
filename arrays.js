27025aa152ba9c1e254f2ef9a9eabf70debc470a
Jonathan Reiland <jreiland@vt.edu>
2017-08-09 00:36:15 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,17 @@
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
+
+function addElementToBeginningOfArray(array, element) {
+  return array.unshift(element);
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  return [element, ...array]
+}
+
+function addElementToEndOfArray(array, element) {
+  return array.push(element);
+}
+
+function destructivelyAddElementToEndOfArray(array, element) {
+  return [...array, element]
+}
