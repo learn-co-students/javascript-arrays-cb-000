b872e61ef5c311298afd610d861b28767ca78dc7
Jen Poole <contact.vandepool@gmail.com>
2018-04-06 04:08:17 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -2,7 +2,7 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 
 /*
-- Most modern browsers support what is called the spread operator
+- Most modern browsers support what is called the `spread operator`
 — it's three dots in a row: (...)
 - when used with an array, it spreads out the array's contents
 - use the spread operator to create a new array in place, rather than modifying the original one
@@ -47,4 +47,32 @@ function addElementToEndOfArray(array, element) {
 function destructivelyAddElementToEndOfArray(array, element) {
   array.push(element);
   return array;
+}
+
+// arrays accessElementInArray(array, index) accesses the element in array at the given index
+function accessElementInArray(array, index) {
+  return array[index];
+}
+
+// arrays destructivelyRemoveElementFromBeginningOfArray(array) returns the array with the first element removed
+// arrays destructivelyRemoveElementFromBeginningOfArray(array) alters the original array
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  array.shift();
+  return array;
+}
+
+/*
+- there is also a way to remove the first element from an array without changing the underlying array; using the `slice` method
+- `slice` does just what its name implies: it takes a slice from its array
+- the first argument specifies where the slice starts, and the second argument specifies where it ends
+- if there is no second argument, the slice goes from the first argument (the start) to the end of the array
+- to remove the first element from an array, use `slice(1)`
+- `slice` is also handy if we know we want the last `n` elements of an array: simply pass it an negative index
+- to remove the last element element from an array, use `slice(-1)`
+*/
+
+// arrays removeElementFromBeginningOfArray(array) removes the first element from the array
+// arrays removeElementFromBeginningOfArray(array) does not alter the original array
+function removeElementFromBeginningOfArray(array) {
+  return array.slice(1);
 }
